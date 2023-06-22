2ec675ee385c5f6548612c98be0e1a24dfe0de65
Jaime Still <jpstill85@gmail.com>
2023-06-22 10:59:56 -0400

    Build-NodeDependencies: finalize cache building and optimizations readme: Add current status and steps. Organize content.

--- lab/hosting-npm/Build-NodeDependencies.ps1
+++ lab/hosting-npm/Build-NodeDependencies.ps1
@@ -1,7 +1,7 @@
 param(
     [string]
     [Parameter()]
-    $Target = ".\.cache",
+    $Target = ".\node_cache",
     [string]
     [Parameter()]
     $Source = ".\package.json",
@@ -9,40 +9,75 @@ param(
     [Parameter()]
     [ValidateSet("npm", "pnpm")]
     $PackageManager = "npm",
+    [string]
+    [Parameter()]
+    $PackTarget = ".\.cache",
+    [switch]
+    [Parameter()]
+    $CleanTarget,
+    [switch]
+    [Parameter()]
+    $CleanPackTarget,
     [switch]
     [Parameter()]
     $PackDependencies
 )
 
+<#
+    Keep track of all packages that are encountered
+    to ensure they are only cached once.
+#>
 $script:packages = @();
 
-function Build-PackageManagerCache {
-    $cache = Join-Path $Target $PackageManager
+<#
+    Creates $dir if it does not exist.
 
-    if (-not (Test-Path $cache)) {
-        New-Item $cache -ItemType Directory -Force | Out-Null
+    If $clean is provided, remove any pre-existing
+    instances of $dir before creation logic.
+#>
+function Build-Directory([string] $dir, [switch] $clean) {
+    if ($clean) {
+        if (Test-Path $dir) {
+            Remove-Item $dir -Recurse -Force
+        }
     }
 
-    return $cache
+    if (-not (Test-Path $dir)) {
+        New-Item $dir -ItemType Directory -Force | Out-Null
+    }
+    
+    return $dir
 }
 
-function Get-DependencyDetails([string] $name, [string] $version) {
-    Write-Host "Retrieving details for $($name)@$($version)"
+<#
+    Get package.json for $name@$version
+#>
+function Get-Package([string] $name, [string] $version) {
+    Write-Host "Retrieving package $name@$version"
     return . npm view "$name@$version" --json | ConvertFrom-Json
 }
 
+<#
+    Use npm to cache $package.name@$package.version to $cache directory
+#>
 function Write-NpmCache([psobject] $package) {
     Write-Host "Caching $($package.name)@$($package.version) to $cache"
     return . npm cache add "$($package.name)@$($package.version)" --cache $cache
 }
 
+<#
+    Use pnpm to cache $package.name@$package.version to pnpm store at $cache
+#>
 function Write-PnpmCache([psobject] $package) {
     Write-Host "Caching $($package.name)@$($package.version) to $cache"
     return . pnpm store add "$($package.name)@$($package.version)" --store-dir $cache
 }
 
+<#
+    Pack the $package.name@$package.version tarball to $path
+#>
 function Write-DependencyPack([psobject] $package) {
-    $path = Join-Path $Target $package.name
+    $path = Join-Path $PackTarget $package.name
 
     if (-not (Test-Path $path)) {
         New-Item $path -ItemType Directory -Force | Out-Null
@@ -52,98 +87,160 @@ function Write-DependencyPack([psobject] $package) {
     . npm pack "$($package.name)@$($package.version)" --pack-destination $path --silent
 }
 
+<#
+    Get all versions of a package that match the semantic version
+    16.1.1  -> 16.1.1 (match exact version)
+    ~16.1.1 -> 16.1.x (flexible patch version)
+    ^16.1.1 -> 16.x.x (flexible major.patch version)
+    See: https://docs.npmjs.com/about-semantic-versioning
+#>
 function Get-SemVersions([string] $name, [string] $semver) {
+    # default filter to exact match
     $filter = '(?<=.?)(\d+\.){2}\d$'
 
+    <#
+        If $semver starts with ^,
+        set filter to match 16.
+    #>
     if ($semver -match '^\^{1}') {
         $filter = '(?<=.?)(\d+\.){1}'
     }
     
+    <#
+        If $semver starts with ~,
+        set filter to match 16.1.
+    #>
     if ($semver -match '^\~{1}') {
         $filter = '(?<=.?)(\d+\.){2}'
     }
 
+    <#
+        Initialize the match filter.
+    #>
     $semver -match $filter | Out-Null
 
+    <#
+        Set filter to match semantic
+        version only for official releases.
+
+        If ~16.1.1:
+        16.1.1          -> match
+        16.1.4          -> match
+        16.2.0          -> no match
+        16.1.4-rc-next  -> no match
+        
+    #>
     $filter = "^$($Matches[0])[\d\.]*$"
 
-    $versions = . npm view $name versions --json | ConvertFrom-Json | Where-Object { $_ -Match $filter }
+    <#
+        Get all versions of the package
+        that match the semantic version filter.
+    #>
+    $versions = . npm view $name versions --json `
+    | ConvertFrom-Json `
+    | Where-Object { $_ -Match $filter }
 
     return $versions
 }
 
-function Write-DependencyCache([string] $name, [string] $semver) {
-    if (-not ($script:packages -contains "$($name)@$($semver)")) {
-        $script:packages += "$($name)@$($semver)"
+<#
+    Handle extended version formats before initiating
+    the cache build to ensure all versions are accounted
+    for.
+
+    This currently only supports any variation of
+    ~<version> || ^<version> || <version>.
+
+    Will need to be extended to handle the following cases:
+    * >version
+    * >=version
+    * <version
+    * <=version
+    * 1.2.x
+    * * or ""
+    * <version> - <version>
+
+    See https://docs.npmjs.com/cli/v9/configuring-npm/package-json#dependencies
+#>
+function Build-Dependencies([psobject] $dependencies) {
+    foreach ($dep in $dependencies.Properties) {
+        if ($dep.Value -match "\|\|") {
+            foreach ($version in $($dep.Value -split "\|\|").Trim()) {
+                Build-DependencyCache $dep.Name $version
+            }
+        }
+        else {
+            Build-DependencyCache $dep.Name $dep.Value
+        }
+    }
+}
+
+<#
+    Cache all relevant versions and its dependencies.
+    If specified, pack the package tarball.
+    Ensures a package is not cached more than once.
+#>
+function Build-DependencyCache([string] $name, [string] $semver) {
+    <#
+        Only proceed if the package has not been encountered yet.
+    #>
+    if (-not ($script:packages -contains "$name@$semver")) {
+        $script:packages += "$name@$semver"
+        Write-Output "Generating dependency cache for $name@$semver"
 
         $versions = Get-SemVersions $name $semver
 
         foreach ($version in $versions) {
-            $details = Get-DependencyDetails $name $version
+            if (-not ($script:packages -contains "$name@$version")) {
+                $script:packages += "$name@$version"
+                $package = Get-Package $name $version
 
-            foreach ($package in $details) {
-                if (-not ($script:packages -contains "$($package.name)@$($package.version)")) {
-                    $script:packages += "$($package.name)@$($package.version)"
-
-                    switch ($PackageManager) {
-                        "npm" { Write-NpmCache $package }
-                        "pnpm" { Write-PnpmCache $package }
-                    }
-
-                    if ($PackDependencies) {
-                        Write-DependencyPack $package
-                    }
-
-                    foreach ($dep in $package.peerDependencies.PSObject.Properties) {
-                        if ($dep.Value -match "\|\|") {
-                            foreach ($version in $($dep.Value -split "\|\|")) {
-                                Write-DependencyCache $dep.Name $version
-                            }
-                        }
-                        else {
-                            Write-DependencyCache $dep.Name $dep.Value
-                        }
-                    }
-
-                    foreach ($dep in $package.dependencies.PSObject.Properties) {
-                        if ($dep.Value -match "\|\|") {
-                            foreach ($version in $($dep.Value -split "\|\|")) {
-                                Write-DependencyCache $dep.Name $version
-                            }
-                        }
-                        else {
-                            Write-DependencyCache $dep.Name $dep.Value
-                        }
-                    }
-
-                    foreach ($dep in $package.optionalDependencies.PSObject.Properties) {
-                        if ($dep.Value -match "\|\|") {
-                            foreach ($version in $($dep.Value -split "\|\|")) {
-                                Write-DependencyCache $dep.Name $version
-                            }
-                        }
-                        else {
-                            Write-DependencyCache $dep.Name $dep.Value
-                        }
-                    }
+                <#
+                    Cache using the appropriate package manager
+                #>
+                switch ($PackageManager) {
+                    "npm" { Write-NpmCache $package }
+                    "pnpm" { Write-PnpmCache $package }
                 }
+
+                if ($PackDependencies) {
+                    Write-DependencyPack $package
+                }
+
+                Build-Dependencies $package.dependencies.PSObject
+                Build-Dependencies $package.peerDependencies.PSObject
+                Build-Dependencies $package.optionalDependencies.PSObject
             }
         }
     }
 }
 
+<#
+    Ensure the specified package manager is installed
+#>
 if (Get-Command $PackageManager) {
+    <#
+        Get the package.json file specified at $Source
+    #>
     $data = Get-Content -Raw -Path $Source | ConvertFrom-Json
+    $guid = (New-Guid).ToString()
 
-    if (Test-Path $Target) {
-        Remove-Item $Target -Recurse -Force
+    Build-Directory $Target $CleanTarget
+    $cache = Build-Directory (Join-Path $Target $PackageManager)
+
+    if ($PackDependencies) {
+        Build-Directory $PackTarget $CleanPackTarget
     }
 
-    $cache = Build-PackageManagerCache
+    Build-Dependencies $data.dependencies.PSObject
+    Build-Dependencies $data.devDependencies.PSObject
+    Build-Dependencies $data.peerDependencies.PSObject
+    Build-Dependencies $data.optionalDependencies.PSObject
 
-    foreach ($dep in $data.dependencies.PSObject.Properties) {
-        Write-DependencyCache $dep.Name $dep.Value
-    }
+    <#
+        Write a file of all packages encountered
+    #>
+    $packages | Sort-Object | Out-File -Append (Join-Path $Target "packages-$guid.txt")
 
     Write-Host "Finished generating Node dependencies!" -ForegroundColor Green
 }
--- lab/hosting-npm/readme.md
+++ lab/hosting-npm/readme.md
@@ -1,20 +1,40 @@
-```powershell
-npm info @angular/core --json
-npm info @angular/core peerDependencies
-npm info rxjs@^7.4.0 dependencies
-npm info tslib@~2.1.0 dist.tarball
+# Hosting npm
 
-mkdir .\.cache\
-Invoke-RestMethod https://registry.npmjs.org/tslib/-/tslib-2.1.0.tgz -OutFile .\.cache\tslib-2.1.0.tgz
+## Current Status - On Hold
 
+I now have the ability to [generate a holistic dependency tree cache](./Build-NodeDependencies.ps1) using both [npm]() and [pnpm](https://pnpm.io/), and can subsequently install offline via the cache. I can also optionally pack the tarballs for each dependency in the tree.
+
+What I have yet to figure out is how to properly establish a central [registry](https://docs.npmjs.com/cli/v8/using-npm/registry) that npm can then be pointed to via [.npmrc](https://docs.npmjs.com/cli/v9/configuring-npm/npmrc).
+
+I initially investigated using [verdaccio](https://verdaccio.org/docs/what-is-verdaccio/) to serve as an offline dependency registry. Out of the box, it serves as a registry proxy to https://registry.npmjs.org/ and interacts with the local cache in the same way as I am building it in the above scripts. 
+
+Relying on the npm cache is inherently not ideal because anytime you need to add packages to it, you cannot cherry pick the new dependencies. You must rebuild the cache as [persistence in the cache is unreliable at best](https://docs.npmjs.com/cli/v8/commands/npm-cache#a-note-about-the-caches-design). It seems promising, but there is more investigation to be done. I'm a little hesitant to rely on another third-party product.
+
+## Next Steps
+
+What would be preferrable would be to establish an HTTP server that is appropriately structured to host the dependencies in their proper structure and format. This would facilitate being able to maintain a centralized, persistent npm registry that can be updated with only new artifacts with each passing update session. See [npm registry](https://docs.npmjs.com/cli/v9/using-npm/registry?v=true#description) and [CommonJS Package Registry](https://wiki.commonjs.org/wiki/Packages/Registry) for more details.
+
+To figure out:
+
+* The format to store npm packages
+* How to serve the npm packages so that the `registry` value can be used for retrieval.
+
+## Scripting Notes
+
+**Use `http-server` to serve a directory**  
+```bash
 npm i -g http-server
 cd .\.cache
 http-server -g
 ```
 
+**retrieve package metadata and download tarball**  
+```powershell
 $ngCore = Invoke-RestMethod https://registry.npmjs.org/@angular/core/latest
-
 Invoke-RestMethod $ngCore.dist.tarball -OutFile .\.cache\@angular\core-16.1.1.tgz
+```
+
+**read a package.json and iterate through the dependencies**  
 ```powershell
 $deps = Get-Content -Raw -Path .\app\package.json | ConvertFrom-Json
 
@@ -22,10 +42,5 @@ foreach ($prop in $deps.dependencies.PSObject.Properties) {
     Write-Output "$($prop.name)@$($prop.Value)"
 }
 ```
-
-To figure out:
-
-* The format to store npm packages
-* How to serve the npm packages so that the `registry` value can be used for retrieval.
 
 `corepack` directory: `$env:LocalAppData\node\corepack`
